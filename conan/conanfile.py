652fad8c4eae91d6f81a87ff5cc9e38a6891e0dc
Carlos Zoido <mrgalleta@gmail.com>
2026-06-05 10:53:57 +0200

    port to conan-py-build

---
+++ conan/conanfile.py
@@ -0,0 +1,24 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+
+
+class LxmlConan(ConanFile):
+    name = "lxml"
+    version = "7.0.0a1"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def layout(self):
+        cmake_layout(self, src_folder="..")
+
+    def requirements(self):
+        self.requires("libxslt/1.1.43")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
